04181e9d8b99ba4b7024cef4f0f3132819ea0b53
unknown <adrianmolina39@gmail.com>
2022-01-19 23:09:27 -0400

    Production

--- src/database/domain/index.js
+++ src/database/domain/index.js
@@ -1,4 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 // Dirección de la DB.
-module.exports = new Sequelize('enmanuel', 'root', '', { host: 'localhost', dialect: 'mysql' });
+// module.exports = new Sequelize('enmanuel', 'root', '', { host: 'localhost', dialect: 'mysql' });
+// module.exports = new Sequelize('enmanuel', 'admin', 'i70iUZs1', { host: 'mysql-66731-0.cloudclusters.net', dialect: 'mysql' });
+module.exports = new Sequelize('centrode_enmanuelle', 'centrode_enmanuelle', 'Enmanuelle2022', {
+    host: 'host.hostingmaracaibo.com',
+    dialect: 'mysql'
+  });

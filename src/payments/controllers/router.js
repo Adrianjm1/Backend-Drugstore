23eb1e62e0cdecf444330b44e52291d2eddcc7d4
Andres <andreseguanipa1@gmail.com>
2022-01-25 16:14:19 -0400

    Payments by bill

--- src/payments/controllers/router.js
+++ src/payments/controllers/router.js
@@ -3,9 +3,9 @@ const { validToken } = require('../../user/controllers/middleware');
 const Controller = require('./index.js');
 
 Router.get('/', validToken, Controller.getAll);
-
 Router.get('/day', validToken, Controller.getPaymentsByDay);
 Router.get('/month', validToken, Controller.getPaymentsByMonth);
+Router.get('/bill', validToken, Controller.getPaymentsByBill);
 Router.post('/create', validToken, Controller.create);
 
 module.exports = Router

61ee30d111c560147be1995a3ec0190ffaaf2d87
unknown <adrianmolina39@gmail.com>
2022-03-09 09:35:40 -0400

    fix

--- src/database/domain/index.js
+++ src/database/domain/index.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require('sequelize');
 
 // Dirección de la DB.
-<<<<<<< HEAD
 // module.exports = new Sequelize('enmanuel', 'root', '', { host: 'localhost', dialect: 'mysql' });
 // module.exports = new Sequelize('enmanuel', 'admin', 'i70iUZs1', { host: 'mysql-66731-0.cloudclusters.net', dialect: 'mysql' });
 // module.exports = new Sequelize('centrode_enmanuelle', 'centrode_enmanuelle', 'Enmanuelle2022', {
@@ -13,6 +12,3 @@ module.exports = new Sequelize('drogueri_enmanuelle', 'drogueri_admin', 'Enmanue
     host: 'host.hostingmaracaibo.com',
     dialect: 'mysql'
   });
-=======
-module.exports = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, { host: process.env.DBHOST, dialect: 'mysql' });
->>>>>>> 3c379459b56c8affecbd9c6c912b9d9c5c1ac020

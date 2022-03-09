3c379459b56c8affecbd9c6c912b9d9c5c1ac020
Andres <andreseguanipa1@gmail.com>
2022-03-09 01:43:23 -0400

    DONE

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+if (process.env.NODE_ENV == 'dev') {
+
+    process.env.DBNAME = 'enmanuel';
+    process.env.DBUSER = 'root';
+    process.env.DBPASSWORD = '';
+    process.env.DBHOST = 'localhost';
+    process.env.SEED = 'token-SEED';
+    process.env.CADUCIDAD_TOKEN = '5h';
+
+}

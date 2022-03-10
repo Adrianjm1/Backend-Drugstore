88eaa4915284947f44561b08dd8a520c3d441317
Andres <andreseguanipa1@gmail.com>
2022-03-10 09:12:45 -0400

    fixing enviroment

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 if (process.env.NODE_ENV == 'dev') {
 
-    process.env.DBNAME = 'drogueri_enmanuelle';
-    process.env.DBUSER = 'drogueri_admin';
-    process.env.DBPASSWORD = 'Enmanuelle2022';
-    process.env.DBHOST = 'host.hostingmaracaibo.com';
+    process.env.DBNAME =  'enmanuel';
+    process.env.DBUSER = 'root';
+    process.env.DBPASSWORD = '';
+    process.env.DBHOST = 'localhost';
     process.env.SEED = 'token-SEED';
     process.env.CADUCIDAD_TOKEN = '5h';
 
